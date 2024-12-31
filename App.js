60dae5e88fc101844ef5c1ae40fe7be17f7e0e27
dylan12manley <dylan.manley@gmail.com>
2024-12-30 21:46:21 -0800

    add access control allow headers, add GET and Post for homePage table

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import express from "express";
+import express from 'express';
 import db from './config/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.json())
+app.use(express.json());
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+  express.json();
+});
 
 app.post('/posts', async (req, res) => {
-  const {title, content, category = null} = req.body;
+  const { title, content, category = null } = req.body;
 
   if (!title && !content) {
     return res.status(400).send('please send title and content');
@@ -21,52 +29,76 @@ app.post('/posts', async (req, res) => {
     return res.status(400).send('please send content for post');
   }
 
-  const [result] = await db.execute(
-    "INSERT INTO posts (title, content, category) VALUES (?,?,?)",
-    [title, content, category]
-  );
+  const [result] = await db.execute('INSERT INTO posts (title, content, category) VALUES (?,?,?)', [title, content, category]);
   res.send({ sucess: result.affectedRows > 0 });
 });
 
 app.get('/posts', async (req, res) => {
   const [results] = await db.execute('SELECT * from posts');
   res.send(results);
-})
+});
 
 app.patch('/posts/:id', async (req, res) => {
-  const {id} = req.params;
-  const {title} = req.body;
+  const { id } = req.params;
+  const { title } = req.body;
 
   if (!title) {
     return res.status(400).send('please send title');
   }
 
-  const [data] =await db.execute("UPDATE posts SET title = ? WHERE id = ?", [
-    title,
-    id,
-  ]);
+  const [data] = await db.execute('UPDATE posts SET title = ? WHERE id = ?', [title, id]);
   res.send({ sucess: data.affectedRows > 0 });
 });
 
 app.delete('/posts/:id', async (req, res) => {
-  const {id} = req.params;
-  const [data] = await db.execute("DELETE FROM posts where postID =?", [id]);
+  const { id } = req.params;
+  const [data] = await db.execute('DELETE FROM posts where postID =?', [id]);
   res.send({ sucess: data.affectedRows > 0 });
-})
-
+});
 
 // USERS
 app.get('/users', async (req, res) => {
-  const [results] = await db.execute('SELECT * from users')
+  const [results] = await db.execute('SELECT * from users');
   res.send(results);
-})
+});
 
 // CODING
 app.get('/coding', async (req, res) => {
-  const [results] = await db.execute('SELECT * from users')
+  const [results] = await db.execute('SELECT * from users');
   res.send(results);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+//
+// FOR JAYA-APP-DB
+// HOME PAGE
+app.get('/homePage', async (req, res) => {
+  const [results] = await db.execute('SELECT * from homePage');
+  res.send(results);
+});
+
+app.post('/homePage', async (req, res) => {
+  const { siteName, homePageH2, homePageH3 = null } = req.body;
+
+  if (!siteName) {
+    return res.status(400).send('siteName homePage');
+  }
+
+  if (!homePageH2) {
+    return res.status(400).send('H2 homePageH2 for homePage');
+  }
+
+  if (!homePageH3) {
+    return res.status(400).send('H3 homePageH3 for homePage');
+  }
+
+  const [result] = await db.execute('INSERT INTO homePage (siteName, homePageH2, homePageH3) VALUES (?,?,?)', [
+    siteName,
+    homePageH2,
+    homePageH3,
+  ]);
+  res.send({ sucess: result.affectedRows > 0 });
 });
